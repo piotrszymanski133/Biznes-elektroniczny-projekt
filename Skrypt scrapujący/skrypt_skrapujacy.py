ab836467b1bb605f8a12efa33c77fc3f1c4373b5
cycv <49570816+cycv@users.noreply.github.com>
2020-11-04 23:07:05 -0500

    Skrypt scrapujący and CSVs update

--- Skrypt scrapujący/skrypt_skrapujacy.py
+++ Skrypt scrapujący/skrypt_skrapujacy.py
@@ -1,3 +1,6 @@
+import sys
+from inspect import getmembers
+
 from selenium import webdriver as wd
 from unidecode import unidecode
 from urllib.parse import unquote
@@ -15,8 +18,21 @@ def generate_dataframe_from_lists(lists: list, column_names: list):
     data = dict(zip(column_names, lists))
     return pd.DataFrame (data, columns=column_names)
 
+def list_duplicates_of(seq,item):
+    start_at = -1
+    locs = []
+    while True:
+        try:
+            loc = seq.index(item,start_at+1)
+        except ValueError:
+            break
+        else:
+            locs.append(loc)
+            start_at = loc
+    return locs
+
 scrap_max_sub_c_number = 5
-scrap_p_number_per_category = 30
+scrap_p_number_per_category = 40
 
 
 ##################################################
@@ -35,41 +51,69 @@ response = rq.get(url)
 if response.status_code == 200:
     html_doc = response.text
     soup = bs(html_doc, 'html.parser')
-    scripts = soup.find_all('script')
-    for script in scripts:
-        # wyciąganie odpowiednich wartości dla kategorii i podkategorii ze znaczników <script>
-        script_id = re.search("'id': '(.*)',", str(script)).group(1) if re.search("'id': '(.*)',", str(script)) \
-                                                                        is not None else ""
-        script_parent = re.search("'parent': '(.*)',", str(script)).group(1) if re.search("'parent': '(.*)',", str(script)) \
-                                                                        is not None else ""
-        script_title = re.search("'title': '(.*)',", str(script)).group(1) if re.search("'title': '(.*)',", str(script)) \
-                                                                        is not None else ""
-        script_url = url + re.search("'url': '(.*)',", str(script)).group(1) if re.search("'url': '(.*)',", str(script)) \
-                                                                        is not None else ""
-        script_hasChildren = re.search("'hasChildren': '(.*)'", str(script)).group(1) if re.search("'hasChildren': '(.*)'", str(script)) \
-                                                                        is not None else ""
+    secondary_id = 2000 # id dla elementów bez id
+    desenio_menuItem = soup.find_all(class_="menuItem")
+    for menuItem in desenio_menuItem:
+        if "show-for-big-tablet-down" in menuItem["class"]:
+            continue
+        menuItem_soup = bs(str(menuItem), 'html.parser')
+        menuItem_href = menuItem_soup.find(class_="descr")["href"]
 
+        if menuItem_href == "/pl/galeria-obrazow": continue     # pominięcie galerii obrazów
 
-        # sprawdzamy czy dana pozycja jest kategorią / podkategorią, która nas interesuje; jesli tak: dodajemy do list
-        if(script_id != "" and script_parent == "" and script_hasChildren != "0" and script_title != "Galerie obrazów") \
-            or (script_parent in c_id):             # Galerie obrazów, choć pasują do wymagań, nie umożliwiają zakupu produktów
+        no_id_category = False
 
-            if len(c_name) > 0 \
-            and script_parent != ""  \
-                and c_name[c_id.index(script_parent)] != "Rozmiary" \
-                    and c_parent.count(script_parent) >= scrap_max_sub_c_number:
-                        continue  # jeśli nie są rozmiarami i rodzic ma już max_sub dzieci nie wykonuj dalej petli i przejdz dalej
-            c_id.append(script_id)
+        scripts = soup.find_all('script')
+        for script in scripts:
+            # wyciąganie odpowiednich wartości dla kategorii i podkategorii ze znaczników <script>
+            script_id = re.search("'id': '(.*)',", str(script)).group(1) if re.search("'id': '(.*)',", str(script)) \
+                                                                            is not None else ""
+            script_parent = re.search("'parent': '(.*)',", str(script)).group(1) if re.search("'parent': '(.*)',", str(script)) \
+                                                                            is not None else ""
+            script_title = re.search("'title': '(.*)',", str(script)).group(1) if re.search("'title': '(.*)',", str(script)) \
+                                                                            is not None else ""
+            script_url = url + re.search("'url': '(.*)',", str(script)).group(1) if re.search("'url': '(.*)',", str(script)) \
+                                                                            is not None else ""
+            script_hasChildren = re.search("'hasChildren': '(.*)'", str(script)).group(1) if re.search("'hasChildren': '(.*)'", str(script)) \
+                                                                            is not None else ""
+            if "rozmiary" in script_url: continue   # rozmiary tylko jako atrybuty, a nie kategoria
+            no_id_category = True
+
+            # sprawdzamy czy dana pozycja jest kategorią / podkategorią, która nas interesuje; jesli tak: dodajemy do list
+            #if(script_id != "" and script_parent == "" and script_hasChildren != "0" and script_title != "Galerie obrazów") \
+            if(menuItem_href in script_url and script_title != "Galerie obrazów"):             # Galerie obrazów, choć pasują do wymagań, nie umożliwiają zakupu produktów
+                if c_parent.count(script_parent) >= scrap_max_sub_c_number \
+                        and (script_parent in c_id and c_name[c_id.index(script_parent)] != "Rozmiary"):
+                    continue
+                if script_parent not in c_id and script_parent != "":
+                    continue
+
+                c_id.append(script_id)
+                c_active.append(1)
+                c_name.append(script_title)
+                if script_parent == "": script_parent = "Strona główna"
+                if script_parent == "171": c_name[-1] = c_name[-1].replace(" ","\u00a0")
+                c_parent.append(script_parent)
+                c_root.append(0)
+                c_url_rewritten.append(re.search("(?s:.*)/(.*)", script_url).group(1))
+                c_img_url.append("")
+
+                print(script_id, script_parent, script_title, script_url, script_hasChildren)
+                if c_parent[-1] not in ['63', '172']:   # nie pobieram informacji s
+                    desenio_c_url.append(script_url)
+                no_id_category = False
+        if no_id_category and "podarunkowa" not in menuItem.text:       # jeżeli brak kategorii tworzymy nowe id
+            c_id.append(secondary_id)
             c_active.append(1)
-            c_name.append(script_title)
-            c_parent.append(script_parent if script_parent != "" else "Strona główna")   # nwm czy dla script_parent == "" dać "Home" !!!!
+            c_name.append(menuItem_soup.text.replace("\n", "").replace("\t", ""))
+            c_parent.append("Strona główna")
             c_root.append(0)
-            c_url_rewritten.append(re.search("(?s:.*)/(.*)", script_url).group(1))
+            c_url_rewritten.append(re.search("(?s:.*)/(.*)", menuItem_href).group(1))
             c_img_url.append("")
+            print(secondary_id, "Strona główna", menuItem_soup.text.replace("\n", "").replace("\t", ""), re.search("(?s:.*)/(.*)", menuItem_href).group(1), "0")
+            secondary_id += 1
+            desenio_c_url.append("https://desenio.pl/" + menuItem_href)
 
-            print(script_id, script_parent, script_title, script_url, script_hasChildren)
-
-            desenio_c_url.append(script_url)
 
 ##########################################################
 # POBIERANIE LINKÓW I ID PRODUKTÓW ZGODNIE Z KATEGORIAMI #
@@ -130,7 +174,7 @@ for url in desenio_c_url:
             p_id.append(id)
             p_categories.append(c_name[desenio_c_url.index(url)])
             desenio_p_url.append(product.a.get("href"))
-            p_price_tax_excluded.append(round(float(product.get("data-price")) / 1.23, 2))
+            p_price_tax_excluded.append(round(float(product.get("data-price")) / 1.23, 6))
     #break   # do wykomentowania po debugu
 driver.close()
 
@@ -145,11 +189,11 @@ comb_price_impact, comb_ecotax, comb_quantity, comb_min_quantity, comb_low_stock
 comb_avail_date, comb_img_postion, comb_img_urls, comb_img_alttexts, comb_shop_id_name, comb_adv_stock_mgmt, \
 comb_dep_on_stock, comb_warehouse = ([] for _ in range(25))
 
-# tworzenie katalogu na pofragmentowanego CSV z danymi o produktach
-csv_part_size = 200
-csv_part_path = 'csv_parts\\'
-try: os.mkdir(csv_part_path)
-except FileExistsError: pass
+# # tworzenie katalogu na pofragmentowanego CSV z danymi o produktach
+# csv_part_size = 200
+# csv_part_path = 'csv_parts\\'
+# try: os.mkdir(csv_part_path)
+# except FileExistsError: pass
 
 # tworzenie katalogu na zdjęcia produktów
 img_catalog_path = "images\\"
@@ -177,7 +221,7 @@ for url in desenio_p_url:
         p_discount_from.append("")
         p_discount_to.append("")
         p_reference.append(p_id[n])
-        p_supplier_ref.append("")             # SUPPLIER??
+        p_supplier_ref.append(re.search("(?s:.*)/(.*)", unidecode(url)).group(1).lower().replace(" ", "-"))             # SUPPLIER??
         p_supplier.append("")                 # SUPPLIER??
         p_manufacturer.append("")             # MANUFACTURER??
         p_ean13.append("")
@@ -206,9 +250,11 @@ for url in desenio_p_url:
         p_meta_keywords.append(soup.find("meta", {"name": "keywords"})["content"])
         p_meta_desc.append(soup.find("meta", {"name": "description"})["content"])
         p_url_rewritten.append(re.search("(?s:.*)/(.*)", unidecode(url)).group(1).lower().replace(" ", "-"))
+        if "ramka" in p_url_rewritten[-1]:  p_url_rewritten[-1] = p_url_rewritten[-1].replace("-13x18", "")
         p_text_in_stock.append(next(iter(soup.find(id="inStock").contents[-2]), "W Magazynie.").replace("\n", ""))
         p_text_backorder.append(next(iter(soup.find(id="outOfStock").contents), "Obecnie zaopatrzenie. Możliwość zamówienia.").replace("\n", "").replace("\t",""))
-        p_avail_order.append(1 if soup.find(id="outOfStock")["class"] == "hidden" else 0)
+        #p_avail_order.append(1 if soup.find("span", {"id": "outOfStock"})["class"] == "hidden" else 0)
+        p_avail_order.append(1)
         p_avail_date.append("")
         p_creation_date.append("")
         p_show_price.append(1)
@@ -244,20 +290,21 @@ for url in desenio_p_url:
             print(p_id[n] + " ERROR: can't download image")
             p_id[n] = "ERROR"
 
-            # wyciąganie rozmiarów i odpowiadających im cen
+
         sizes, prices = [], []
         prices_sizes_script = soup.find(text=re.compile("'size': '(.*m)"))
         for line in prices_sizes_script.string.split('\n'):
             if "'size':" in line:
-                sizes.append(re.search("'size': '(.*(?s:.*)m)", str(line)).group(1))
+                size = re.search("'size': '(.*(?s:.*)m)", str(line)).group(1)
+                sizes.append(size if size.find(" ") > 0 else size[:-2] + " " + size[-2:])
             if "defaultPrice = " in line:
                 prices.append(re.search("defaultPrice = '(.*)'", str(line)).group(1))
-        sizes.sort(key=lambda x: re.search("\d+", x).group(0))
+        sizes.sort(key=lambda x: int(re.search("\d+", x).group(0)))
         prices.sort(key=lambda x: int(x))
 
         # tworzenie każdej możliwej kombinacji
         for i in range(len(sizes)):
-            comb_p_id.append(p_id[n])
+            comb_p_id.append(n+1)
             comb_p_reference.append(p_reference[n])
             comb_attribute.append("Rozmiar:select:0")
             comb_value.append(sizes[i] + ":0")              #
@@ -266,7 +313,7 @@ for url in desenio_p_url:
             comb_ean13.append(p_ean13[n])
             comb_upc.append(p_upc[n])
             comb_wholesale_price.append(p_wholesale_price[n])
-            comb_price_impact.append(round(float(prices[i]) / 1.23 - p_price_tax_excluded[n], 2))   #
+            comb_price_impact.append(round(float(prices[i]) / 1.23 - p_price_tax_excluded[n], 6))   #
             comb_ecotax.append(p_ecotax[n])
             comb_quantity.append(p_quantity[n])             # idk jaką wartość
             comb_min_quantity.append(p_min_quantity[n])
@@ -275,7 +322,7 @@ for url in desenio_p_url:
             comb_weight_impact.append(round(i*0.1, 2))                # tu tez nwm
             comb_default.append(1 if i == 0 else 0)         #
             comb_avail_date.append(p_avail_date[n])
-            comb_img_postion.append(i)                     # idk jaką wartość tutaj
+            comb_img_postion.append(1)                     # idk jaką wartość tutaj
             comb_img_urls.append(p_img_urls[n])
             comb_img_alttexts.append(p_img_alttexts[n])
             comb_shop_id_name.append(p_shop_id_name[n])
@@ -283,6 +330,38 @@ for url in desenio_p_url:
             comb_dep_on_stock.append(p_dep_on_stock[n])
             comb_warehouse.append(p_warehouse[n])
     print(p_id[n], p_name[n], p_categories[n])
+
+#RAMKA POSTPROCESS (XD)
+for i in range(len(p_id)):
+    if "Ramka" in p_name[i]:
+        p_name[i] = re.search("(.*) \d+x\d+", p_name[i].replace(",", "")).group(1)
+        if "Ramka drewniane białe" in p_name[i]:
+            p_name[i] = "Ramka drewniana biała"
+        print(i, p_name[i])
+
+k = len(p_id)
+for i in range(k):
+    print(i, k)
+    if i < k:
+        print(p_name[i])
+        if "Ramka" in p_name[i]:
+            postprocess_duplicates = list_duplicates_of(p_name, p_name[i])
+            if len(postprocess_duplicates) > 1:
+                print(postprocess_duplicates)
+                for dup in reversed(postprocess_duplicates[1:]):
+                    comb_price_impact[comb_p_reference.index(p_id[dup])] = p_price_tax_excluded[dup] - \
+                                                                           p_price_tax_excluded[
+                                                                               postprocess_duplicates[0]]
+                    comb_p_id[comb_p_reference.index(p_id[dup])] = i + 1            #indeks głownego produktu w prestashopie
+                    comb_p_reference[comb_p_reference.index(p_id[dup])] = p_id[i]   # zmiana id referowanego produktu
+                    [o[1].pop(dup) for o in getmembers(sys.modules[__name__]) if o[0].startswith('p_')]
+                    k -= 1
+
+for i in range(len(comb_p_reference)):
+    comb_p_id[i] = p_id.index(comb_p_reference[i]) + 1
+    if "70X100" in comb_value[i]: comb_value[i] = comb_value[i].replace("70X100", "70x100")     # poprawa błędu na desenio 'X' -> 'x'
+    if "ramka" in comb_ref[i] and "13x18" not in comb_ref[i]: comb_default[i] = 0               # wszystkie ramki prócz 13x18 nie są defaultowe
+
 # tworzenie plików CSV
 list_c = [c_id, c_active, c_name, c_parent, c_root, c_desc, c_meta_title, c_meta_keywords, c_meta_desc, c_url_rewritten, c_img_url]
 list_p = [p_id, p_active, p_name, p_categories, p_price_tax_excluded, p_tax_rules_id, p_wholesale_price, p_on_sale, p_discount_amount, p_discount_percent, p_discount_from, p_discount_to, p_reference, p_supplier_ref, p_supplier, p_manufacturer, p_ean13, p_upc, p_ecotax, p_width, p_height, p_depth, p_weight, p_delivery_time_in_stock, p_delivery_time_out_stock, p_quantity, p_min_quantity, p_low_stock_lvl, p_send_email_quantity, p_visibility, p_shipping_cost, p_unity, p_unit_price, p_summary, p_desc, p_tags, p_meta_title, p_meta_keywords, p_meta_desc, p_url_rewritten, p_text_in_stock, p_text_backorder, p_avail_order, p_avail_date, p_creation_date, p_show_price, p_img_urls, p_img_alttexts, p_img_delete, p_feature, p_avail_online_only, p_condition, p_customizable, p_uploadable_files, p_text_fields, p_out_action, p_virtual, p_file_url, p_nr_dl, p_exp_date, p_nr_of_days, p_shop_id_name, p_adv_stock_mgmt, p_dep_on_stock, p_warehouse, p_acessories]
