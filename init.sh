1c8d3a15ca235c6fd959f647f5ec16a687eb959c
werkaflute <63801151+werkaflute@users.noreply.github.com>
2021-01-02 16:34:04 +0100

    Add pod-script

--- init.sh
+++ init.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-DB="db_2"
-USER="user_2"
-PASSWORD="matibob333"
-DB_ROOT_PASSWORD="dev"
+kubectl --namespace="biznes-elektroniczny" cp ~/studenci/2/BEProjekt/prestashop.sql biznes-db-7d46b58f4d-rm2v9:/tmp/prestashop.sql
 
-#kubectl --namespace="biznes-elektroniczny" cp ~/studenci/2/BEProjekt/prestashop.sql biznes-db-7d46b58f4d-rm2v9:/tmp/prestashop.sql
+kubectl --namespace="biznes-elektroniczny" cp ~/studenci/2/BEProjekt/pod-script.sh biznes-db-7d46b58f4d-rm2v9:/tmp/pod-script.sh
 
-kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB};" 
-kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${USER}@localhost IDENTIFIED BY '${PASSWORD}';" 
-kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';" 
-kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;" 
-kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -u $USER -p$PASSWORD $DB < ~/studenci/2/BEProjekt/prestashop.sql
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- /tmp/pod-script.sh 
---
+++ pod-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DB="db_2"
+USER="user_2"
+PASSWORD="matibob333"
+DB_ROOT_PASSWORD="dev"
+
+mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB};" 
+mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${USER}@localhost IDENTIFIED BY '${PASSWORD}';" 
+mysql -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';" 
+mysql -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;" 
+mysql -u $USER -p$PASSWORD $DB < /tmp/prestashop.sql
