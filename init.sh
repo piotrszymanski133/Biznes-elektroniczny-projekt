7e9fc45309d4201d75cbbe9516a928f01afbd42d
werkaflute <63801151+werkaflute@users.noreply.github.com>
2021-01-02 16:16:37 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,9 +7,8 @@ DB_ROOT_PASSWORD="dev"
 
 kubectl --namespace="biznes-elektroniczny" cp ~/studenci/2/BEProjekt/prestashop.sql biznes-db-7d46b58f4d-rm2v9:/tmp/prestashop.sql
 
-kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- \ 
-mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB};" && \
-mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${USER}@localhost IDENTIFIED BY '${PASSWORD}';" && \
-mysql -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';" && \
-mysql -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;" && \
-mysql -u $USER -p$PASSWORD $DB < /tmp/prestashop.sql 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB};" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${USER}@localhost IDENTIFIED BY '${PASSWORD}';" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -u $USER -p$PASSWORD $DB < /tmp/prestashop.sql 
