7a6075969577204f8eef23a55bf53ad9ba4c73f8
werkaflute <63801151+werkaflute@users.noreply.github.com>
2021-01-07 16:09:42 +0100

    Final

--- db/init.sh
+++ db/init.sh
@@ -3,13 +3,12 @@
 DB="db_2"
 USER="user_2"
 PASSWORD="matibob333"
+DB_ROOT_PASSWORD="dev"
 
-sleep 5
+#kubectl --namespace="biznes-elektroniczny" cp ~/studenci/2/BEProjekt/prestashop.sql biznes-db-7d46b58f4d-rm2v9:/tmp/prestashop.sql
 
-mysql -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB};"
-mysql -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${USER}@localhost IDENTIFIED BY '${PASSWORD}';"
-mysql -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';"
-mysql -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-git clone -b database --single-branch https://github.com/Quetzonarch/BEProjekt.git
-mysql -u $USER -p$PASSWORD $DB < ./BEProjekt/prestashop.sql
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${DB};" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS ${USER}@localhost IDENTIFIED BY '${PASSWORD}';" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;" 
+kubectl --namespace="biznes-elektroniczny" exec --stdin --tty biznes-db-7d46b58f4d-rm2v9 -- mysql -u $USER -p$PASSWORD $DB < ~/studenci/2/BEProjekt/prestashop.sql
